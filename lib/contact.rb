0921a545a0418fea70a4a48113428d0be5fdb0ad
hlinforest <hllin@sfu.ca>
2015-07-08 22:34:58 +0000

    fixed bug

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ActiveRecord::Base
   validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
--- main.rb
+++ main.rb
@@ -12,9 +12,7 @@ end
 def new_contact
   puts "Enter email: "
   input3 = gets.chomp
-  # if Contact.find_by("email = ?", input3)
-  #   puts "Error: Record already exist"
-  # else
+  
   puts "Enter first name: "
   input1 = gets.chomp
   puts "Enter last name: "
@@ -23,21 +21,94 @@ def new_contact
   #end
 end
 
-def search
+def update_contact
+  puts "Enter email: "
+  input3 = gets.chomp
   
+  temp1 = Contact.find_by("email = ?", input3)
+  if temp1
+    puts "Enter new email: "
+    input3 = gets.chomp
+    puts "Enter new first name: "
+    input1 = gets.chomp
+    puts "Enter new last name: "
+    input2 = gets.chomp
+    temp1.update(first_name: input1, last_name: input2, email: input3)
+  else
+    puts "Error: record does not exist"
+  end
+
 end
-puts "Contact List App V2"
+
+def search
+  puts "Enter new email: "
+  input3 = gets.chomp
+  temp1 = Contact.find_by("email = ?", input3)
+  if temp1
+    puts "Full name: #{temp1.first_name} #{temp1.last_name}"
+    puts "Email: #{temp1.email}"
+  else
+    puts "Error: record does not exist"
+  end
+end
+
+def delete_contact
+  puts "Enter new email: "
+  input3 = gets.chomp
+  temp1 = Contact.find_by("email = ?", input3)
+  if temp1
+    temp1.destroy
+  else
+    puts "Error: record does not exist"
+  end
+end
+
+def find_by_first_name
+  puts "Enter first name:"
+  input1 = gets.chomp
+  temp1 = Contact.where("first_name = ?", input1)
+  temp1.each do |temp2|
+    puts "Full name: #{temp2.first_name} #{temp2.last_name}"
+    puts "Email: #{temp2.email}"
+  end
+
+end
+
+def find_by_last_name
+  puts "Enter last name:"
+  input1 = gets.chomp
+  temp1 = Contact.where("last_name = ?", input1)
+  temp1.each do |temp2|
+    puts "Full name: #{temp2.first_name} #{temp2.last_name}"
+    puts "Email: #{temp2.email}"
+  end
+
+end
+
+def find_emails_by_domain
+  puts "Enter email:"
+  input1 = gets.chomp
+  temp1 = Contact.where("email LIKE ?", "%#{input1}%")
+  temp1.each do |temp2|
+    puts "Full name: #{temp2.first_name} #{temp2.last_name}"
+    puts "Email: #{temp2.email}"
+  end
+
+end
+
+puts "Contact List App V3"
 loop do
   print %q(
   Menu
 
   list - show all contact
   create - create a new contact
-  search - find contact by last name or first name or email
+  search - find contact by email
   update - update contact
   delete - delete contact
-  find last name - find contact by last name
-  find first name - find contact by first name
+  find by last name - return list of contact by last name
+  find by first name - return list of contact by first name
+  find emails by domain - return list of contact by email
   quit - exit program
   > )
 
@@ -50,12 +121,16 @@ loop do
     update_contact
   when "delete"
     delete_contact
-  when "find first name"
-    find_first_name
-  when "find last name"
-    find_last_name
+  when "find by first name"
+    find_by_first_name
+  when "find by last name"
+    find_by_last_name
+  when "find emails by domain"
+    find_emails_by_domain
   when "list"
     list
+  when "search"
+    search
   when "quit"
     exit
   else
